fe29262c79d2c75c702a8f8d4444c934815d9b7e
Warren <warrendsteinacker@gmail.com>
2026-02-17 11:49:26 -0500

    nnn

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,10 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: './', // Change this to relative so it works everywhere
+//   server: {
+//     port: 3000,
+//   }
+// })
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Change this to relative so it works everywhere
-  server: {
-    port: 3000,
-  }
+  base: '/this-is-my-website-for-el/', // This matches your repo name exactly
 })
