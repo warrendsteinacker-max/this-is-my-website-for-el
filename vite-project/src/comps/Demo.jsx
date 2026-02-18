4b0637c54c5a6dd64e14477a6419ab9a92b57523
Warren <warrendsteinacker@gmail.com>
2026-02-18 13:45:48 -0500

    adding new comp

---
+++ vite-project/src/comps/Demo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Demo = () => {
+  return (
+    <div style={{height: '100vh', width: '100wh', backgroundColor: 'whitesmoke'}}>Demo</div>
+  )
+}
+
+export default Demo
