6891b42f765c874737c0d3b158ff34611f43a519
Warren <warrendsteinacker@gmail.com>
2026-02-18 14:28:16 -0500

    creating another testing styles comp

--- vite-project/src/comps/Demo2.jsx
+++ vite-project/src/comps/Demo2.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Demo2 = () => {
   return (
-    <div style></div>
+    <div style={{widht: '100vw', height: '100vh', backgroundColor: 'white', position: 'relitive'}}><div className='cir'></div></div>
   )
 }
 
