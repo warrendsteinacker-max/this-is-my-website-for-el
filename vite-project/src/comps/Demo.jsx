5e338eeda23587437588358677c7be4ba2f67efa
Warren <warrendsteinacker@gmail.com>
2026-02-18 13:54:35 -0500

    adding new styles

--- vite-project/src/comps/Demo.jsx
+++ vite-project/src/comps/Demo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Demo = () => {
   return (
-    <div style={{height: '100vh', width: '100wh', backgroundColor: 'whitesmoke'}}>Demo</div>
+    <div style={{height: '100vh', width: '100wh', backgroundColor: 'whitesmoke', postion: 'reletive'}}><div className="sin"></div></div>
   )
 }
 
